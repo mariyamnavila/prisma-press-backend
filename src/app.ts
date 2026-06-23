5f9ca229ba15a2082797c0dfe7ac9d777cc90f94
mariyamnavila <bibimariyamnavila@gmail.com>
2026-06-24 03:28:49 +0600

    made setup

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Application, Request, Response } from "express";
+import config from "./config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { prisma } from "./lib/prisma";
+
+const app: Application = express();
+
+app.use(cors({
+    origin: config.app_url,
+    credentials: true,
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", async (req: Request, res: Response) => {
+    const user = await prisma.user.findMany();
+    console.log(user);
+    res.send("Hello, World!");
+});
+
+export default app;
